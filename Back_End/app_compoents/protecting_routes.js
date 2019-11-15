ac66e87c021393e09ac6512a19668cbf40338934
Xiangshuai Gao <xiangshuai.gao@stonybrook.edu>
2019-11-15 02:47:48 -0500

    updated

--- Back_End/app_compoents/protecting_routes.js
+++ Back_End/app_compoents/protecting_routes.js
@@ -4,7 +4,7 @@ function not_authen_redirect(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
-    res.redirect('/login');
+    return res.redirect('/login');
 }
 
 function authen_redirect(req, res, next){
